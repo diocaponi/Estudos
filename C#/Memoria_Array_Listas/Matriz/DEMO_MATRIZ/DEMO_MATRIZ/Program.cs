37446da333226eb4e4d883f99f536b0a8c8ef283
Diovanni Caponi <44592828+diocaponi@users.noreply.github.com>
2021-01-22 17:34:13 -0300

    Demonstração MATRIZ

---
+++ C#/Memoria_Array_Listas/Matriz/DEMO_MATRIZ/DEMO_MATRIZ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DEMO_MATRIZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaração matriz
+            double[,] mat = new double[2, 3];
+            
+            // LENGTH - Quantos elementos tem no total
+            Console.WriteLine(mat.Length);
+            
+            // RANK - Primeira dimensão matriz, no caso qtde de linhas
+            Console.WriteLine(mat.Rank);
+
+            // GETLENGTH - Mais o parametro, exibe a dimensao da matriz 
+            Console.WriteLine(mat.GetLength(0));
+            
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
