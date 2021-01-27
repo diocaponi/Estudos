c41e700ef95567aabe198098c964966912e1ac08
Diovanni Caponi <44592828+diocaponi@users.noreply.github.com>
2021-01-26 23:35:45 -0300

    Exercício Matriz

---
+++ C#/Memoria_Array_Listas/Matriz/EX1/EX1/EX1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a ordem da matriz: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            Console.WriteLine("Digite a matriz separando os elementos por espaço e as linhas por ENTER: ");
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("Diagonal principal:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Número negativos: " + count);
+        }
+    }
+}
