01060e6232a1f76d7728ce111a21cca878444ee8
Diovanni Caponi <44592828+diocaponi@users.noreply.github.com>
2021-01-27 15:30:08 -0300

    Exercício Switch_Case

---
+++ C#/Logica_Programacao/Switch_Case/EX1/EX1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace EX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número (1 a 7) para retornar o dia da semana: ");
+            int x = int.Parse(Console.ReadLine());
+
+            string dia;
+
+            switch (x)
+            {
+                case 1:
+                    dia = "Domingo";
+                    break;
+                case 2:
+                    dia = "Segunda";
+                    break;
+                case 3:
+                    dia = "Terça";
+                    break;
+                case 4:
+                    dia = "Quarta";
+                    break;
+                case 5:
+                    dia = "Quinta";
+                    break;
+                case 6:
+                    dia = "Sexta";
+                    break;
+                case 7:
+                    dia = "Sábado";
+                    break;
+                default:
+                    dia = "Número inválido";
+                    break;
+            }
+
+            Console.WriteLine("Dia: " + dia);
+        }
+    }
+}
